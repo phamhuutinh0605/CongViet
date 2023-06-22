78bec5df28cece8c279325f6cf40a91daea39842
huutinh <phamhuutinh801@gmail.com>
2023-06-22 17:07:22 +0700

    ádfasdfasdf

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   baseURL: "https://api-congviet.onrender.com/api",
+  // baseURL: "http://localhost:8800/api",
   withCredentials: true,
   // headers: {
   //   Authorization: "Bearer " + token
